fa0a111a766ac5c976a781036da3d64275d27f39
Victor Mehmeri <vmehmeri@hotmail.com>
2016-06-23 03:09:39 -0500

    Fixed configuration script

--- configure_compute_node.sh
+++ configure_compute_node.sh
@@ -9,13 +9,15 @@ if [ $# -ne 2 ]; then
 fi
 
 cd ~
+
+rm -rf devstack sfc-random
 git clone -b stable/liberty https://github.com/openstack-dev/devstack 
 git clone https://github.com/trozet/sfc-random.git 
 
 
 cp -f devstack-lab/compute.conf ./devstack/local.conf
-sed -i 's/1.2.3.4/$CONTROLLER_IP_ADDRESS/g'
-sed -i 's/0.0.0.0/$IP_ADDRESS/g'
+sed -i 's/1.2.3.4/'$CONTROLLER_IP_ADDRESS'/g' ./devstack/local.conf
+sed -i 's/0.0.0.0/'$IP_ADDRESS'/g' ./devstack/local.conf
 
 sudo ovs-vsctl set-manager tcp:$CONTROLLER_IP_ADDRESS:6640
 
